9ade1f0df6a995c0481ada977f2e45c5ce43d22c
MorihisaT <morihisa87go@gmail.com>
2020-11-02 20:52:49 +0900

    ミスの修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,10 @@
 class EventsController < ApplicationController
   def new
-    @event = current_user.created_event.build
+    @event = current_user.created_events.build
   end
 
   def create
-    @event = current_user.created_event.build(event_params)
+    @event = current_user.created_events.build(event_params)
 
     if @event.save
       redirect_to @event, notice: "作成しました"
@@ -12,7 +12,7 @@ class EventsController < ApplicationController
   end
 
   private
-  
+
   def event_params
     params.require(:event).permit(:name, :place, :content, :start_at, :end_at)
   end
