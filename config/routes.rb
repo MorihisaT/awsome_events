3812c6ab9d256d444406521c483b1f55c3ce4011
MorihisaT <morihisa87go@gmail.com>
2020-11-06 22:22:06 +0900

    ミスの修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :events
   root "welcome#index"
   get "/auth/:provider/callback" => "sessions#create"
-  delete "/logout" => "sessions#destroy"
+  delete "/logout" => "sessions#destroy", as: :logout
 
   resources :retirements
 
